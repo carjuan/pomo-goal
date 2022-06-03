cf0e49a6f8a7af63cca974ea4398d1c48048d9f1
Juanse <pcjvan@gmail.com>
2022-06-02 22:11:21 -0500

    feat: add HTMLElements selectors

---
+++ src/dom/index.js
@@ -0,0 +1,5 @@
+const timerHTMLElement = document.querySelector("[data-is=timer]");
+const playButtonHTMLElement = document.querySelector("[data-is=play-button]");
+const pauseButtonHTMLElement = document.querySelector("[data-is=pause-button]");
+
+export { timerHTMLElement, playButtonHTMLElement, pauseButtonHTMLElement };
